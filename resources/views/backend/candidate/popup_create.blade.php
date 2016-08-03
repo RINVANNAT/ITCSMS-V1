7cb1f1bbd2c00d51d8a35ee838ef0f53f646522d
thavorac_gic <thavorak@itc.edu.kh>
2016-08-03 15:00:00 +0700

    try to fix notify

--- resources/views/backend/candidate/popup_create.blade.php
+++ resources/views/backend/candidate/popup_create.blade.php
@@ -54,24 +54,7 @@
     <script>
 
         function notify_message(type, message, title){
-            toastr.options = {
-                "closeButton": true,
-                "debug": false,
-                "newestOnTop": false,
-                "progressBar": false,
-                "positionClass": "toast-bottom-right",
-                "preventDuplicates": false,
-                "onclick": null,
-                "showDuration": "300",
-                "hideDuration": "1000",
-                "timeOut": "5000",
-                "extendedTimeOut": "1000",
-                "showEasing": "swing",
-                "hideEasing": "linear",
-                "showMethod": "fadeIn",
-                "hideMethod": "fadeOut"
-            }
-            toastr[type](message, title);
+            alert('khkhlklkhlk');
         }
 
         function save_candidate(){
