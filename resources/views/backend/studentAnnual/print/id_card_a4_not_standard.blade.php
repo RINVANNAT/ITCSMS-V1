6672d3f0c0acc6481b633816bc814eb61b825cd6
thavorac <thavorac.chun@gmail.com>
2016-12-21 10:49:27 +0700

    update card

--- resources/views/backend/studentAnnual/print/id_card_a4_not_standard.blade.php
+++ resources/views/backend/studentAnnual/print/id_card_a4_not_standard.blade.php
@@ -248,7 +248,7 @@
 
                 @foreach($rows as $row)
 
-                    <div class="row" style="margin:0px; padding-right: 1cm !important; padding-top: 2mm !important;">
+                    <div class="row" style="margin:0px; padding-top: 2mm !important;">
                         <?php $row = array_reverse($row); //dd($row);?>
                         @foreach($row as $back)
 
