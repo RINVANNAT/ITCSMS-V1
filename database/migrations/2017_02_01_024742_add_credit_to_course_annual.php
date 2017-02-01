e7152bcc66ea585b76d1ea90264c781dac594998
thavorac <thavorac.chun@gmail.com>
2017-02-01 09:51:42 +0700

    add credit to course annual

---
+++ database/migrations/2017_02_01_024742_add_credit_to_course_annual.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddCreditToCourseAnnual extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('course_annuals', function (Blueprint $table) {
+            $table->double('credit');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('course_annuals', function (Blueprint $table) {
+            //
+        });
+    }
+}
