105417766ac0e3df532af59b9599bd37753ad35f
thavorac <thavorac.chun@gmail.com>
2017-02-01 10:15:13 +0700

    update table employees

---
+++ database/migrations/2017_02_01_031105_add_column_status_id_card_to_employees.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddColumnStatusIdCardToEmployees extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('employees', function (Blueprint $table) {
+            $table->integer('id_card')->nullable();
+            $table->string('status')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('employees', function (Blueprint $table) {
+            //
+        });
+    }
+}
