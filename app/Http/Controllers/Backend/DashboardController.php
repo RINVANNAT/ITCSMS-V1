0939abbd78f9d28ce04e42b7f6209fc2e1e0e4b6
thavorac <thavorac.chun@gmail.com>
2017-02-14 09:39:42 +0700

    Fix some bugs and create migration for column responsible department

--- app/Http/Controllers/Backend/DashboardController.php
+++ app/Http/Controllers/Backend/DashboardController.php
@@ -27,12 +27,12 @@ class DashboardController extends Controller
 
         if($employee != null){
             $last_year = AcademicYear::orderBy('id','DESC')->first();
-            $courses = CourseAnnual::leftJoin('course_annual_classes', 'course_annual_classes.course_annual_id', '=', 'course_annuals.id')
-                ->leftJoin('departments','course_annual_classes.department_id', '=', 'departments.id')
-                ->leftJoin('degrees','course_annual_classes.degree_id', '=', 'degrees.id')
-                ->leftJoin('grades','course_annual_classes.grade_id', '=', 'grades.id')
-                ->where('academic_year_id',$last_year->id)
+            $courses = CourseAnnual::leftJoin('departments','course_annuals.department_id', '=', 'departments.id')
+                ->leftJoin('degrees','course_annuals.degree_id', '=', 'degrees.id')
+                ->leftJoin('grades','course_annuals.grade_id', '=', 'grades.id')
+                ->where('course_annuals.academic_year_id',$last_year->id)
                 ->where('employee_id',$employee->id)
+                ->with("courseAnnualClass")
                 ->select([
                     'course_annuals.name_en',
                     'course_annuals.id',
