b4c4ac702037111eb2ddd8dc3b99d27127c7ff50
HEL Mab <mabhelitc@gmail.com>
2017-10-09 11:44:38 +0700

    Add teacher view and edit timetable permission.

--- resources/views/backend/dashboard/includes/timetable.blade.php
+++ resources/views/backend/dashboard/includes/timetable.blade.php
@@ -12,130 +12,6 @@
                 <div class="panel-heading">
                     <form action="/admin/dashboard/get_teacher_timetable" method="POST" id="form_teacher_timetable">
                         <div class="pull-left">
-
-                            {{--@if(isset($academicYears))
-                                <select name="academicYears"
-                                        data-toggle="tooltip"
-                                        data-placement="top"
-                                        title="Academic Year">
-                                    @foreach($academicYears as $index => $academicYear)
-                                        @if($index==0)
-                                            <option value="{{ $academicYear->id }}"
-                                                    selected>{{ $academicYear->name_latin }}</option>
-                                        @else
-                                            <option value="{{ $academicYear->id }}">{{ $academicYear->name_latin }}</option>
-                                        @endif
-                                    @endforeach
-                                </select>
-                            @endif
-
-                            @if(isset($departments))
-                                <select name="departments"
-                                        data-toggle="tooltip"
-                                        data-placement="top"
-                                        title="Department">
-                                    @foreach($departments as $index => $department)
-                                        @if($index==0)
-                                            <option value="{{ $department->id }}"
-                                                    selected>{{ $department->code }}</option>
-                                        @else
-                                            <option value="{{ $department->id }}">{{ $department->code }}</option>
-                                        @endif
-                                    @endforeach
-                                </select>
-                            @endif
-
-                            @if(isset($degrees))
-                                <select name="degrees"
-                                        data-toggle="tooltip"
-                                        data-placement="top"
-                                        title="Degree">
-                                    @foreach($degrees as $index => $degree)
-                                        @if($index==0)
-                                            <option value="{{ $degree->id }}" selected>{{ $degree->name_en }}</option>
-                                        @else
-                                            <option value="{{ $degree->id }}">{{ $degree->name_en }}</option>
-                                        @endif
-                                    @endforeach
-                                </select>
-                            @endif
-
-                            @if(isset($grades))
-                                <select name="grades"
-                                        data-toggle="tooltip"
-                                        data-placement="top"
-                                        title="Grade">
-                                    @foreach($grades as $index => $grade)
-                                        @if($index==0)
-                                            <option value="{{ $grade->id }}" selected>{{ $grade->name_en }}</option>
-                                        @else
-                                            <option value="{{ $grade->id }}">{{ $grade->name_en }}</option>
-                                        @endif
-                                    @endforeach
-                                </select>
-                            @endif
-
-                            @if(isset($options))
-                                <select name="options"
-                                        data-toggle="tooltip"
-                                        data-placement="top"
-                                        title="Option">
-                                    @foreach($options as $index => $option)
-                                        @if($index==0)
-                                            <option value="{{ $option->id }}" selected>{{ $option->name_en }}</option>
-                                        @else
-                                            <option value="{{ $option->id }}">{{ $option->name_en }}</option>
-                                        @endif
-                                    @endforeach
-                                </select>
-                            @endif
-
-                            @if(isset($semesters))
-                                <select name="semesters"
-                                        data-toggle="tooltip"
-                                        data-placement="top"
-                                        title="Semester">
-                                    @foreach($semesters as $index => $semester)
-                                        @if($index==0)
-                                            <option value="{{ $semester->id }}"
-                                                    selected>{{ $semester->name_en }}</option>
-                                        @else
-                                            <option value="{{ $semester->id }}">{{ $semester->name_en }}</option>
-                                        @endif
-                                    @endforeach
-                                </select>
-                            @endif
-
-                            @if(isset($weeks))
-                                <select name="weeks"
-                                        data-toggle="tooltip"
-                                        data-placement="top"
-                                        title="Week">
-                                    @foreach($weeks as $index => $week)
-                                        @if($index==0)
-                                            <option value="{{ $week->id }}" selected>{{ $week->name_en }}</option>
-                                        @else
-                                            <option value="{{ $week->id }}">{{ $week->name_en }}</option>
-                                        @endif
-                                    @endforeach
-                                </select>
-                            @endif
-
-                            @if(isset($groups))
-                                <select name="groups"
-                                        data-toggle="tooltip"
-                                        data-placement="top"
-                                        title="Group">
-                                    @foreach($groups as $index => $group)
-                                        @if($index==0)
-                                            <option value="{{ $group->id }}" selected>{{ $group->code }}</option>
-                                        @else
-                                            <option value="{{ $group->id }}">{{ $group->code }}</option>
-                                        @endif
-                                    @endforeach
-                                </select>
-                            @endif--}}
-
                             <select name="timetable_id" id="teacher_timetable" class="form-control"
                                     style="width: 300px;">
                                 @foreach($timetables as $timetable)
@@ -176,22 +52,24 @@
 
                         </div>
 
-                        <div class="pull-right"
-                             data-toggle="tooltip"
-                             data-placement="top"
-                             title="Mode">
-                            <input type="checkbox"
-                                   name="filter_by"
-                                   checked data-toggle="toggle"
-                                   class="form-control"
-                                   data-on="View"
-                                   data-off="Edit"
-                                   data-width="100"
-                                   data-height="25"
-                                   data-onstyle="primary"
-                                   data-offstyle="warning"
-                                   data-style="ios">
-                        </div>
+                        @if(access()->allow('teacher-edit-timetable'))
+                            <div class="pull-right"
+                                 data-toggle="tooltip"
+                                 data-placement="top"
+                                 title="Mode">
+                                <input type="checkbox"
+                                       name="filter_by"
+                                       checked data-toggle="toggle"
+                                       class="form-control"
+                                       data-on="View"
+                                       data-off="Edit"
+                                       data-width="100"
+                                       data-height="25"
+                                       data-onstyle="primary"
+                                       data-offstyle="warning"
+                                       data-style="ios">
+                            </div>
+                        @endif
                     </form>
 
                     <div class="clearfix"></div>
