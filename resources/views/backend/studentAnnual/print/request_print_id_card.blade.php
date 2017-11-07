4bbd3896573548ef2ca44f5c19f2b216cc8dee7c
thavorac <thavorac.chun@gmail.com>
2017-11-07 10:55:57 +0700

    Update max id cards to print

--- resources/views/backend/studentAnnual/print/request_print_id_card.blade.php
+++ resources/views/backend/studentAnnual/print/request_print_id_card.blade.php
@@ -195,7 +195,7 @@
 @section('content')
 
     <div class="box box-success">
-        @if(sizeof($studentAnnuals_front) > 100)
+        @if(sizeof($studentAnnuals_front) > 200)
         {{--<div class="alert alert-danger alert-dismissible">--}}
             {{--<button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>--}}
             {{--<h4><i class="icon fa fa-ban"></i> Error!</h4>--}}
