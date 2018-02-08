89a08136567babf6e606e0b8d73e7f228aa94773
HEL Mab <mabhelitc@gmail.com>
2018-02-08 15:33:54 +0700

    Fix show a single timetable.

--- app/Http/Controllers/Backend/Schedule/TimetableController.php
+++ app/Http/Controllers/Backend/Schedule/TimetableController.php
@@ -324,7 +324,6 @@ class TimetableController extends Controller
                 'timetable_slots.id',
                 'timetable_slots.course_name as title',
                 'timetable_slots.course_name',
-                'timetable_slots.teacher_name',
                 'timetable_slots.type as type',
                 'timetable_slots.start',
                 'timetable_slots.end',
@@ -334,29 +333,10 @@ class TimetableController extends Controller
             ->get();
         $timetableSlots = new Collection();
 
-        if ($timetable->department_id < 12 && ($timetable instanceof Timetable)) {
-            // get student annuals id
-            $student_annual_ids = $this->timetableSlotRepo->find_student_annual_ids($timetable);
-            $department_languages = array(12, 13); // (english, french)
-            foreach ($department_languages as $department_language) {
-                // get group language, [@return array(Collection $groups, Array $groups)]
-                $groups = $this->timetableSlotRepo->get_group_student_annual_form_language($department_language, $student_annual_ids, $timetable);
-
-                // get timetable language,
-                $timetables = $this->timetableSlotRepo->get_timetables_form_language_by_student_annual($groups[0], $timetable, $department_language);
-
-                // get timetable slots [@return array(timetableSlots, groupsRoom)]
-                $timetableSlotsLang = $this->timetableSlotRepo->get_timetable_slot_language_dept($timetables, $groups[0]);
-
-                // set timetable slots language to view.
-                $this->timetableSlotRepo->set_timetable_slot_language($timetableSlots, $timetableSlotsLang[1], $timetableSlotsLang[0]);
-            }
-        }
-
         foreach ($timetable_slots as $timetable_slot) {
             if (($timetable_slot instanceof TimetableSlot) && is_object($timetable_slot)) {
 
-                $newTimetableSlot = TimetableSlot::find($timetable_slot->id);
+                $newTimetableSlot = TimetableSlot::with('employee')->find($timetable_slot->id);
                 $timetableSlot = new Collection($newTimetableSlot);
                 $timetableSlot->put('building', $timetable_slot->building);
                 $timetableSlot->put('room', $timetable_slot->room);
@@ -364,7 +344,6 @@ class TimetableController extends Controller
             }
         }
 
-        //dd($timetable);
         $options = [
             $timetable->academic_year_id,
             $timetable->department_id,
@@ -375,7 +354,6 @@ class TimetableController extends Controller
             isset($timetable->group_id) ? $timetable->group_id : 0,
             $timetable->week_id
         ];
-
 
         return view('backend.schedule.timetables.show', compact('timetableSlots', 'timetable', 'now', 'createTimetablePermissionConfiguration', 'options'));
     }
