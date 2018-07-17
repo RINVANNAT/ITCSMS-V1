d1a6b7f37f23f6fc6afa981c381f75a233e64a07
HEL Mab <mabhelitc@gmail.com>
2018-07-17 10:41:32 +0700

    add suppoters number.

--- resources/views/frontend/auth/login.blade.php
+++ resources/views/frontend/auth/login.blade.php
@@ -13,37 +13,37 @@
 
                     {!! Form::open(['url' => 'login', 'class' => 'form-horizontal']) !!}
 
-                        <div class="form-group">
-                            {!! Form::label('email', trans('validation.attributes.frontend.email'), ['class' => 'col-md-4 control-label']) !!}
-                            <div class="col-md-6">
-                                {!! Form::input('email', 'email', null, ['class' => 'form-control', 'placeholder' => trans('validation.attributes.frontend.email')]) !!}
-                            </div><!--col-md-6-->
-                        </div><!--form-group-->
+                    <div class="form-group">
+                        {!! Form::label('email', trans('validation.attributes.frontend.email'), ['class' => 'col-md-4 control-label']) !!}
+                        <div class="col-md-6">
+                            {!! Form::input('email', 'email', null, ['class' => 'form-control', 'placeholder' => trans('validation.attributes.frontend.email')]) !!}
+                        </div><!--col-md-6-->
+                    </div><!--form-group-->
 
-                        <div class="form-group">
-                            {!! Form::label('password', trans('validation.attributes.frontend.password'), ['class' => 'col-md-4 control-label']) !!}
-                            <div class="col-md-6">
-                                {!! Form::input('password', 'password', null, ['class' => 'form-control', 'placeholder' => trans('validation.attributes.frontend.password')]) !!}
-                            </div><!--col-md-6-->
-                        </div><!--form-group-->
+                    <div class="form-group">
+                        {!! Form::label('password', trans('validation.attributes.frontend.password'), ['class' => 'col-md-4 control-label']) !!}
+                        <div class="col-md-6">
+                            {!! Form::input('password', 'password', null, ['class' => 'form-control', 'placeholder' => trans('validation.attributes.frontend.password')]) !!}
+                        </div><!--col-md-6-->
+                    </div><!--form-group-->
 
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <div class="checkbox">
-                                    <label>
-                                        {!! Form::checkbox('remember') !!} {{ trans('labels.frontend.auth.remember_me') }}
-                                    </label>
-                                </div>
-                            </div><!--col-md-6-->
-                        </div><!--form-group-->
+                    {{--<div class="form-group">
+                        <div class="col-md-6 col-md-offset-4">
+                            <div class="checkbox">
+                                <label>
+                                    {!! Form::checkbox('remember') !!} {{ trans('labels.frontend.auth.remember_me') }}
+                                </label>
+                            </div>
+                        </div><!--col-md-6-->
+                    </div><!--form-group-->--}}
 
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                {!! Form::submit(trans('labels.frontend.auth.login_button'), ['class' => 'btn btn-primary', 'style' => 'margin-right:15px']) !!}
+                    <div class="form-group">
+                        <div class="col-md-6 col-md-offset-4">
+                            {!! Form::submit(trans('labels.frontend.auth.login_button'), ['class' => 'btn btn-primary', 'style' => 'margin-right:15px']) !!}
 
-                                {!! link_to('password/reset', trans('labels.frontend.passwords.forgot_password')) !!}
-                            </div><!--col-md-6-->
-                        </div><!--form-group-->
+                            {{--{!! link_to('password/reset', trans('labels.frontend.passwords.forgot_password')) !!}--}}
+                        </div><!--col-md-6-->
+                    </div><!--form-group-->
 
                     {!! Form::close() !!}
 
@@ -55,6 +55,45 @@
             </div><!-- panel -->
 
         </div><!-- col-md-8 -->
+
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading"><i class="fa fa-bullhorn"></i> System Supporters</div>
+                <div class="panel-body">
+                    <table class="table table-hover table-striped">
+                        <thead>
+                        <tr>
+                            <th>No.</th>
+                            <th>Name</th>
+                            <th>Telephone</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>Mr. KIM YOUNENG</td>
+                            <td><a href="tel:010 73 76 40">010 73 76 40</a></td>
+                        </tr>
+                        <tr>
+                            <td>2</td>
+                            <td>Miss. EUT CHANLEAKENA</td>
+                            <td><a href="tel:010 85 51 99">010 85 51 99</a></td>
+                        </tr>
+                        <tr>
+                            <td>3</td>
+                            <td>Mr. CHUN THAVORAC</td>
+                            <td><a href="tel:096 60 00 880">096 60 00 880</a></td>
+                        </tr>
+                        <tr>
+                            <td>4</td>
+                            <td>Mr. HEL Mab</td>
+                            <td><a href="tel:096 24 49 031">096 24 49 031</a></td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
 
     </div><!-- row -->
 
--- resources/views/frontend/includes/nav.blade.php
+++ resources/views/frontend/includes/nav.blade.php
@@ -36,7 +36,7 @@
 
                 <!-- Authentication Links -->
                 @if (Auth::guest())
-                    <li>{!! link_to('login', trans('navs.frontend.login')) !!}</li>
+                    {{--<li>{!! link_to('login', trans('navs.frontend.login')) !!}</li>--}}
                 @else
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
