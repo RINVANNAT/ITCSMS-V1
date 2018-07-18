56b930b5f9e98c1d030481a93d29e00dc94ba100
thavorac <thavorac.chun@gmail.com>
2018-07-18 12:16:29 +0700

    [TASK] Average score list

--- app/Http/Controllers/Backend/StudentTrait/AverageFinalYearTrait.php
+++ app/Http/Controllers/Backend/StudentTrait/AverageFinalYearTrait.php
@@ -28,6 +28,11 @@ trait AverageFinalYearTrait
         $department_option = null;
         $degree = null;
         $academic_year = null;
+        $department_id = $_GET["department_id"];
+        $option_id = $_GET["option_id"];
+        $degree_id= $_GET["degree_id"];
+        $academic_year_id = $_GET["academic_year_id"];
+
         if($_GET["department_id"] != "") {
             $department = Department::find($_GET["department_id"]);
         }
@@ -41,6 +46,35 @@ trait AverageFinalYearTrait
             $academic_year = AcademicYear::find($_GET["academic_year_id"]);
         }
         $semester = 2;
+
+        $graduated_student_ids = Student::select([
+                "students.id"
+            ])
+            ->leftJoin('studentAnnuals','students.id','=','studentAnnuals.student_id')
+            ->leftJoin('academicYears', 'studentAnnuals.academic_year_id', '=', 'academicYears.id')
+            ->leftJoin('group_student_annuals', 'group_student_annuals.student_annual_id', '=', 'studentAnnuals.id')
+            ->where(function($query){
+                $query->where('students.radie','=', false)->orWhereNull('students.radie');
+            })
+            ->whereNull('group_student_annuals.department_id')
+            ->where("studentAnnuals.academic_year_id","=",$academic_year->id)
+            ->where(function($query) use($semester){
+                $query->where("group_student_annuals.semester_id",$semester)->orWhereNull("group_student_annuals.semester_id");
+            });
+
+        if($department_option != null){
+            $graduated_student_ids = $graduated_student_ids->where('studentAnnuals.department_option_id',"=",$department_option->id);
+        }
+        if($department != null){
+            $graduated_student_ids = $graduated_student_ids->where('studentAnnuals.department_id',"=",$department->id);
+        }
+        if($degree != null){
+            $graduated_student_ids = $graduated_student_ids->where('studentAnnuals.degree_id',"=",$degree->id);
+        }
+        $graduated_student_ids = $graduated_student_ids->where(function($query) {
+            $query->where('studentAnnuals.grade_id',"=", 5)
+                ->orWhere('studentAnnuals.grade_id',"=", 2);
+        })->pluck("id");
 
         $students  = Student::select([
             'students.id_card',
@@ -88,7 +122,7 @@ trait AverageFinalYearTrait
                 $query->where('students.radie','=', false)->orWhereNull('students.radie');
             })
             ->whereNull('group_student_annuals.department_id')
-            ->whereIN("studentAnnuals.academic_year_id",[$academic_year->id,$academic_year->id-1])
+            //->whereIN("studentAnnuals.academic_year_id",[$academic_year->id,$academic_year->id-1])
             ->where(function($query) use($semester){
                 $query->where("group_student_annuals.semester_id",$semester)->orWhereNull("group_student_annuals.semester_id");
             });
@@ -107,12 +141,11 @@ trait AverageFinalYearTrait
                 ->orWhere('studentAnnuals.grade_id',"=", 5)
                 ->orWhere('studentAnnuals.grade_id',"=", 1)
                 ->orWhere('studentAnnuals.grade_id', 2);
-            });
+            })->whereIN('students.id',$graduated_student_ids);
 
             $students = $students->orderBy('students.id_card','ASC')
             ->get()
             ->toArray();
-
         $students = collect($students);
         $student_by_groups = collect($students)->sortBy(function($student){
             return sprintf('%-12s%s',
@@ -122,22 +155,61 @@ trait AverageFinalYearTrait
         })->groupBy("student_id");
 
         $scores = [];
-
+        // Clean redouble student record first
+        foreach($student_by_groups as &$student_by_class){
+            $before_graduated_year = null;
+            $before_graduated_key = null;
+            $graduated_year = null;
+            $graduated_key = null;
+            if(count($student_by_class) > 2) {
+                foreach($student_by_class as $key => $student_by_grade) {
+                    if($student_by_grade['grade_id'] == 4 || $student_by_grade['grade_id']==1){
+                        if($before_graduated_year !== null) {
+                            // already exist, compare which one is smaller then remove
+                            if($before_graduated_year>$student_by_grade['academic_year_id']){
+                                $student_by_class->forget($key);
+                            } else {
+                                $student_by_class->forget($before_graduated_key);
+                            }
+                        } else {
+                            $before_graduated_key = $key;
+                            $before_graduated_year = $student_by_grade['academic_year_id'];
+                        }
+                    } else if ($student_by_grade['grade_id'] == 5 || $student_by_grade['grade_id']==2) {
+                        if($graduated_year !== null) {
+                            // already exist, compare which one is smaller then remove
+                            if($graduated_year>$student_by_grade['academic_year_id']){
+                                $student_by_class->forget($key);
+                            } else {
+                                $student_by_class->forget($graduated_key);
+                            }
+                        } else {
+                            $graduated_key = $key;
+                            $graduated_year = $student_by_grade['academic_year_id'];
+                        }
+                    }
+                }
+            }
+        }
+        $errors = [];
         foreach($student_by_groups as $student_by_class){
             if(count($student_by_class) == 2) {
                 foreach($student_by_class as $student_by_grade) {
                     $scores[$student_by_grade["id"]] = $this->getStudentScoreBySemester($student_by_grade['id'],null); // Full year
+                    if(empty($scores[$student_by_grade["id"]])) {
+                        $scores[$student_by_grade["id"]] = array("final_score" => "N/A","final_score_s1" => "N/A","final_score_s2" => "N/A");
+                    }
                 }
             } else {
                 // Something wrong here. It suppose to have only 2
-                //dd($student_by_class);
+                array_push($errors,array("count"=>count($student_by_class), "id" => $student_by_class));
                 //throw new \Exception('Students have multiple class record');
             }
         }
 
         if ($type == "show"){
-            return view('backend.studentAnnual.average_final_year', compact('student_by_groups','scores','department','department_option','degree','academic_year'));
+            return view('backend.studentAnnual.average_final_year', compact('student_by_groups','scores','department','department_option','degree','academic_year','department_id','option_id','degree_id','academic_year_id'));
         }
-        return PDF::loadView('backend.studentAnnual.print.average_final_year')->setPaper('a4')->stream();
+        return PDF::loadView('backend.studentAnnual.print.average_final_year', compact('student_by_groups','scores','department','department_option','degree','academic_year'))->setPaper('a4')->stream();
     }
 }
