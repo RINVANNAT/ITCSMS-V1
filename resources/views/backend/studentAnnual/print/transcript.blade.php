a9c58db9e46fa7398a4aca820f9ae4dbdeba9dc2
HEL Mab <mabhelitc@gmail.com>
2018-08-14 16:28:27 +0700

    update ui.

--- resources/views/backend/studentAnnual/print/transcript.blade.php
+++ resources/views/backend/studentAnnual/print/transcript.blade.php
@@ -22,7 +22,7 @@
                     <div class="transcript-header">
                         <table class="head">
                             <tr>
-                                <td colspan="2"><h4><b>Department1:</b> <span class="no-bold">{{$student['department_en']}}</span></h4></td>
+                                <td colspan="2"><h4><b>Department:</b> <span class="no-bold">{{$student['department_en']}}</span></h4></td>
                             </tr>
                             @if($student['option_en'] != "" && ($student['department_id'] != 2 && $student['degree_id'] != 2 && $student['grade_id'] != 2 ))
                                 <tr>
