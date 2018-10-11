8a019a35d9ff33e738661aee070c00e6409615f4
HEL Mab <mabhelitc@gmail.com>
2018-10-11 17:11:04 +0700

    timetable ui.

--- resources/views/backend/schedule/timetables/create.blade.php
+++ resources/views/backend/schedule/timetables/create.blade.php
@@ -34,6 +34,20 @@
         .bg-red {
             background-color: #dd4b39 !important;
         }
+
+        .info-box {
+            width: 250px !important;
+            display: inline-block;
+            margin-right: 15px;
+            cursor: pointer;
+            margin-bottom: 0 !important;
+        }
+        .tab-content {
+            overflow-x: auto;
+            white-space: nowrap;
+            padding: 10px !important;
+            background: #ffffff !important;
+        }
     </style>
 
 @stop
@@ -116,17 +130,45 @@
 
                 <div class="box-body">
                     <div class="row">
-                        <div class="col-md-9 col-sm-12 col-xs-12">
+                        <div class="col-md-12">
                             <div id="timetable" class="view-timetable"></div>
                         </div>
-                        <div class="col-md-3 col-sm-12 col-xs-12">
-                            @include('backend.schedule.timetables.includes.partials.courses-sessions')
-                            @include('backend.schedule.timetables.includes.partials.rooms')
-                        </div>
+                        {{--<div class="col-md-12">--}}
+                            {{--@include('backend.schedule.timetables.includes.partials.courses-sessions')--}}
+                            {{--@include('backend.schedule.timetables.includes.partials.rooms')--}}
+                        {{--</div>--}}
                     </div>
                     <div class="clearfix"></div>
                 </div>
                 <div class="panel-conflict box box-danger" id="conflict" style="display: none;"></div>
+            </div>
+        </div>
+        <div class="sticky-wrapper">
+            <ul class="nav nav-tabs" role="tablist">
+                <li role="presentation" class="active"><a href="#home" aria-controls="home" role="tab" data-toggle="tab">Home</a></li>
+                <li role="presentation"><a href="#profile" aria-controls="profile" role="tab" data-toggle="tab">Profile</a></li>
+                <li role="presentation"><a href="#messages" aria-controls="messages" role="tab" data-toggle="tab">Messages</a></li>
+                <li role="presentation"><a href="#settings" aria-controls="settings" role="tab" data-toggle="tab">Settings</a></li>
+            </ul>
+
+            <!-- Tab panes -->
+            <div class="tab-content">
+                <div role="tabpanel" class="tab-pane active" id="home">
+                    <div class="info-box" v-for="item in 10"><span class="info-box-icon bg-aqua"><span class="room_name">A-110</span></span><div class="info-box-content"><span class="info-box-number">Library</span><span class="info-box-number room_id hidden">117</span><span class="info-box-text text-muted">N/A Desk</span><span class="info-box-text text-muted">N/A Chair</span></div></div>
+                </div>
+                <div role="tabpanel" class="tab-pane" id="profile">
+                    <div class="info-box" v-for="item in 10"><span class="info-box-icon bg-aqua"><span class="room_name">A-110</span></span><div class="info-box-content"><span class="info-box-number">Library</span><span class="info-box-number room_id hidden">117</span><span class="info-box-text text-muted">N/A Desk</span><span class="info-box-text text-muted">N/A Chair</span></div></div>
+                </div>
+                <div role="tabpanel" class="tab-pane" id="messages">
+                    <div class="info-box" v-for="item in 10"><span class="info-box-icon bg-aqua"><span class="room_name">A-110</span></span><div class="info-box-content"><span class="info-box-number">Library</span><span class="info-box-number room_id hidden">117</span><span class="info-box-text text-muted">N/A Desk</span><span class="info-box-text text-muted">N/A Chair</span></div></div>
+                </div>
+                <div role="tabpanel" class="tab-pane" id="settings">
+                    <div class="info-box" v-for="item in 10"><span class="info-box-icon bg-aqua"><span class="room_name">A-110</span></span><div class="info-box-content"><span class="info-box-number">Library</span><span class="info-box-number room_id hidden">117</span><span class="info-box-text text-muted">N/A Desk</span><span class="info-box-text text-muted">N/A Chair</span></div></div>
+                </div>
+            </div>
+
+            <div class="sticky-content">
+                {{--<div class="info-box" v-for="item in 10"><span class="info-box-icon bg-aqua"><span class="room_name">A-110</span></span><div class="info-box-content"><span class="info-box-number">Library</span><span class="info-box-number room_id hidden">117</span><span class="info-box-text text-muted">N/A Desk</span><span class="info-box-text text-muted">N/A Chair</span></div></div>--}}
             </div>
         </div>
     </div>
