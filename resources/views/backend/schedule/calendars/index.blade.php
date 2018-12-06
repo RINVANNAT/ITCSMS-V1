7b75cbeda3cdc538bfdc64b4f78b6a5a8336ddec
Chanleakena <eutchanleakena@gmail.com>
2018-12-06 17:11:36 +0700

    Calendar

--- resources/views/backend/schedule/calendars/index.blade.php
+++ resources/views/backend/schedule/calendars/index.blade.php
@@ -66,81 +66,34 @@
     <!-- <script type="text/javascript" src="{{ asset('js/backend/schedule/calendar.js') }}"></script> -->
     <!-- <script type="text/javascript" src="{{ asset('plugins/datetimepicker/bootstrap-datetimepicker.min.js') }}"></script> -->
     <script type="text/javascript">
-    $(function() {
-        $('#calendar').fullCalendar({
-            schedulerLicenseKey: 'CC-Attribution-NonCommercial-NoDerivatives',
-            editable: true,
-            selectable: true,
-            aspectRatio: 1,
-            scrollTime: '00:00',
-            header: {
-                left: 'promptResource today prev,next',
-                center: 'title',
-                right: 'timelineThreeDays'
-            },
-            customButtons: {
-            promptResource: {
-                text: '+ room',
-                click: function() {
-                var title = prompt('Room name');
-                if (title) {
-                    $('#calendar').fullCalendar(
-                    'addResource',
-                    { title: title },
-                    true // scroll to the new resource?
-                    );
-                }
-                }
-            }
-            },
-            defaultView: 'timelineThreeDays',
-            defaultDate: '2018-12-01',
-            nowIndicator: true,
-            locale: 'en',
-            displayEventTime: true,
-            displayEventEnd: true,
-            now: '2018-12-02T09:25:00',
-            views: {
-                timelineThreeDays: {
-                    type: 'timeline',
-                    minTime: "07:00:00",
-                    maxTime: "17:00:00",
-                    duration: { days: 7 },
-                    slotDuration: '00:30:00',
-                    slotLabelFormat: [
-                        'dddd, DD/MM/YY', // top level of text
-                        'h(:mm)a'        // lower level of text
-                    ]
-                    // slotDuration: {hours: 6},
-                    // snapDuration: {hours: 24},
-                }
-            },
-            eventOverlap: false,
-            resourceGroupField: 'building',
-            resourceAreaWidth: '25%',
-            resourceLabelText: 'Rooms',
-            resources: [
-                { id: 'a', building: 'Building A', title: 'A102' },
-                { id: 'b', building: 'Building A', title: 'A103', eventColor: 'green' },
-                { id: 'c', building: 'Building A', title: 'A104', eventColor: 'orange' },
-                { id: 'd', building: 'Building B', title: 'B200'},
-                { id: 'e', building: 'Building B', title: 'B202' },
-                { id: 'f', building: 'Building F', title: 'F301', eventColor: 'red' },
-                { id: 'g', building: 'Building F', title: 'F401' },
-                { id: 'h', building: 'Building F', title: 'F404' },
-                { id: 'i', building: 'Building I', title: 'I206' },
-                { id: 'j', building: 'Building I', title: 'I208' },
-                { id: 'k', building: 'Building I', title: 'I209' }
-            ],
-            events: [
-                { id: '1', resourceId: 'a', start: '2018-12-01T12:00:00', end: '2018-12-01T13:30:00', title: 'I3GIC' },
-                { id: '2', resourceId: 'b', start: '2018-12-02T12:00:00', end: '2018-12-03T12:00:00', title: 'I5GIM-A' },
-                { id: '3', resourceId: 'e', start: '2018-12-01', end: '2018-12-04', title: 'event 3' },
-                { id: '4', resourceId: 'f', start: '2018-12-02T12:00:00', end: '2018-12-05T12:00:00', title: 'I5GIM-B' },
-                { id: '5', resourceId: 'g', start: '2018-12-01T12:00:00', end: '2018-12-06T12:00:00', title: 'I3GEE Calculus' }
-            ]
+
+        $(function() {
+            var date = new Date();
+            var d = date.getDate(),
+                m = date.getMonth(),
+                y = date.getFullYear();
+            $('#calendar').fullCalendar({
+                defaultView: 'timetable',
+                defaultDate: '2017-01-01',
+                header: false,
+                footer: false,
+                views: {
+                    timetable: {
+                        type: 'agendaWeek',
+                        setHeight: '100px'
+                    }
+                },
+                allDaySlot: false,
+                hiddenDays: [0],
+                height: 735,
+                fixedWeekCount: false,
+                minTime: '07:00:00',
+                maxTime: '20:00:00',
+                slotLabelFormat: 'h:mm a',
+                columnFormat: 'dddd',
+                timezone: 'Asia/Phnom_Penh',
+            });
         });
-    });
     </script>
 
 @stop
