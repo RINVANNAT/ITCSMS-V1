e8ec7243af043f91c88ee9ffbb94024f058b1d87
HEL Mab <mabhelitc@gmail.com>
2019-02-26 18:57:52 +0700

    reset password.

--- resources/views/frontend/auth/emails/password.blade.php
+++ resources/views/frontend/auth/emails/password.blade.php
@@ -1,1 +1,10 @@
-{{ trans('strings.emails.reset_password') }}: {{ url('password/reset/'.$token) }}
+<p>Seems like you forgot your password for SMIS System. if this is true, click below to reset your password</p>
+
+
+<p>Click here to reset your password: {{ url('password/reset/'.$token) }}</p>
+
+
+<p>Thankfully</p>
+
+
+<p>SMIS Team</p>
