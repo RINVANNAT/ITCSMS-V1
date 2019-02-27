2ffa260491acf93ffbfcb5eac37f39d0ccf0dc66
HEL Mab <mabhelitc@gmail.com>
2019-02-27 08:40:32 +0700

    reset password email.

--- resources/views/frontend/auth/emails/password.blade.php
+++ resources/views/frontend/auth/emails/password.blade.php
@@ -1,4 +1,4 @@
-<h1>Hello!</h1>
+<p>Dear Sir/Madam,</p>
 <p>You are receiving this email because we received a password reset request for your account</p>
 <br/>
 <p>Click here to reset your password: {{ url('password/reset/'.$token) }}</p>
