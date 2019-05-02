b6caac1fa1b3d3cf198e29951d542545175723af
Chanleakena <eutchanleakena@gmail.com>
2019-05-02 09:48:21 +0700

    Fix print cergificate font

--- resources/views/backend/studentAnnual/print/foundation_certificate.blade.php
+++ resources/views/backend/studentAnnual/print/foundation_certificate.blade.php
@@ -210,7 +210,7 @@
                             <p class="tran-header text-center">បញ្ជាក់ថា​ ៖</p>
                             <div class="row" style="margin-top: 10mm">
                                 <div class="col-md-6">
-                                    <p class="left">និស្សិតឈ្មោះ&nbsp;&nbsp;&nbsp;៖ <span style="font-family: 'khmeros_bold' !important; font-size: 14px">{{$student['name_kh']}}</span></p>
+                                    <p class="left">និស្សិតឈ្មោះ&nbsp;&nbsp;&nbsp;៖ <strong><span style="font-family: khmer_s1 !important; font-size: 14px">{{$student['name_kh']}}</span></strong></p>
                                 </div>
                                 <div class="col-md-6">
                                     <p class="right" style="margin-right: 9px">ភេទ៖ {{to_khmer_gender($student['gender'])}}</p>
